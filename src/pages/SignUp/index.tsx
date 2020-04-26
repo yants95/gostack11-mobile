2b1d5924a2157fa78f9d00dd1cc64c2a907a269b
Yan Soares <yts.1995@gmail.com>
2020-04-26 15:59:36 -0300

    feat: integrating signup with api

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import * as Yup from 'yup';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import api from '../../services/api';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -53,9 +54,14 @@ const SignUp: React.FC = () => {
                 abortEarly: false,
             });
 
-            // await api.post('/users', data);
+            await api.post('/users', data);
 
-            // history.push('/');
+            Alert.alert(
+                'Cadastro realizado com sucesso!',
+                'Vocâ já pode fazer login na aplicação',
+            );
+
+            navigation.goBack();
         } catch (err) {
             if (err instanceof Yup.ValidationError) {
                 const errors = getValidationErrors(err);
@@ -128,7 +134,7 @@ const SignUp: React.FC = () => {
                             <Button
                                 onPress={() => formRef.current?.submitForm()}
                             >
-                                Entrar
+                                Cadastrar
                             </Button>
                         </Form>
                     </Container>
